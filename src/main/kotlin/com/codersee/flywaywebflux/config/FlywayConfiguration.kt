3b46b17f428418d961084cfec74e80acd301da9d
Piotr Wolak <contact@codersee.com>
2022-05-31 07:38:02 +0200

    Flyway configuration with @Value annotation

--- src/main/kotlin/com/codersee/flywaywebflux/config/FlywayConfiguration.kt
+++ src/main/kotlin/com/codersee/flywaywebflux/config/FlywayConfiguration.kt
@@ -1,21 +1,22 @@
 package com.codersee.flywaywebflux.config
 
 import org.flywaydb.core.Flyway
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
 
 @Configuration
-class FlywayConfiguration(private val env: Environment) {
+class FlywayConfiguration(
+    @Value("\${spring.flyway.url}") private val url: String,
+    @Value("\${spring.flyway.user}") private val user: String,
+    @Value("\${spring.flyway.password}") private val password: String
+) {
 
     @Bean(initMethod = "migrate")
     fun flyway(): Flyway {
         return Flyway(
             Flyway.configure()
-                .dataSource(
-                    env.getRequiredProperty("spring.flyway.url"),
-                    env.getRequiredProperty("spring.flyway.user"),
-                    env.getRequiredProperty("spring.flyway.password"))
+                .dataSource(url, user, password)
         )
     }
 }
